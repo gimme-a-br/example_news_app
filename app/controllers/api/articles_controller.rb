806f5c36497512a5e6228d184ae8e36fc3f83d8a
Peter Jang <peter.x.jang@gmail.com>
2020-09-22 20:01:43 -0500

    Add action to display news articles and weather from external APIs

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,2 +1,13 @@
 class Api::ArticlesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{Rails.application.credentials.news_api[:api_key]}")
+    data = response.parse
+    articles = data["articles"]
+    titles = articles.map { |article| article["title"] }
+
+    response = HTTP.get("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=#{Rails.application.credentials.weather_api[:api_key]}")
+    data = response.parse
+
+    render json: { titles: titles, my_weather: data }
+  end
 end
